f53170725e6dcd57d24b9412439797ba8ab6d40c
MichaelB167 <michael.m.bates@gmail.com>
2016-07-07 23:58:36 -0400

    Create linked list

--- linked-list.js
+++ linked-list.js
@@ -1,4 +1,6 @@
+/*jslint node: true */
 'use strict';
+
 
 const linkedList = function(val) {
   let node = new Node(val),
@@ -9,10 +11,38 @@ const linkedList = function(val) {
     this._length++;
     return node;
   };
+
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = node;
 };
 
-const makeNode = (data, next) => {
-  return {
-    data,
-  };
+// second version
+
+const makeNode = (data, next) => ({ data, next});
+
+let list = makeNode('first');
+
+list = makeNode('second', list);
+
+list = makeNode('third', list);
+
+const printList = (list) => {
+  for (let node = list; node; node = node.next) {
+    console.log(node.data);
+  }
 };
+
+printList(list);
+
+// third version
+
+list.prototype.prepend = function
+list.append('first');
+list.append('second');
+list.append('third');
+list.prepend('before first');
+
+const printList = (list) =>
